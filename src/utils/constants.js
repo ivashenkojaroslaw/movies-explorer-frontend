0c0a7cf5f2e6279c436c943f961764816aa5402a
Yaroslav Ivashenko <yaroslav.@mail.ru>
2021-08-25 21:44:23 +0300

    fix: change main_url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const MOVIES_API_URL = 'https://api.nomoreparties.co';
 
-//export const MAIN_API_URL = 'https://api.diplom.back.nomoredomains.monster';
+export const MAIN_API_URL = 'https://api.diplom.back.nomoredomains.monster';
 
-export const MAIN_API_URL = 'http://localhost:3000';
+//export const MAIN_API_URL = 'http://localhost:3000';
 
 export const DURATION_SHORT_MOVIE = 40;
 export const NUMBER_SHOWED_MOVIES = 5;
